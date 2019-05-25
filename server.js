7a6b2e36e86742f80424e6bfcc8a3775b59a873a
ruslan <ruslan.palagin.ck@gmail.com>
2019-05-25 22:56:57 +0300

    #master: upd msg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World with yarn!';
+  ctx.body = 'Hello World with yarn! Powered by GCP';
 });
 
 app.listen(8080);
