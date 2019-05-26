13b4c4abb42c31fd870adc20aa594b2d6f63c2bc
ruslan <ruslan.palagin.ck@gmail.com>
2019-05-26 21:25:36 +0300

    #master: connect to gcloud sql locally

--- server.js
+++ server.js
@@ -7,5 +7,3 @@ app.use(homeAction);
 
 app.listen(8080);
  
-// instance1 8pM6oDOgbkyxuMH9 https://cloud.google.com/sql/docs/postgres/connect-app-engine?hl=ru
-//  gcloud sql connect instance1 --user=postgres
--- src/instances/knex.js
+++ src/instances/knex.js
@@ -1,25 +1,44 @@
 // const knex = require('knex')({
+//     debug: true,
 //     client: 'pg',
 //     connection: {
 //       host : '/cloudsql/docker-test-241719:europe-west6:instance1',
 //       user : 'postgres',
 //       password : '8pM6oDOgbkyxuMH9',
 //       database : 'ecommerce_staging'
-//     }
+//     },
 //   });
 
 // module.exports = knex;
+
+// const knex = require('knex')({
+//     debug: true,
+//     client: 'pg',
+//     connection: {
+//       host : 'localhost',
+//       user : 'postgres',
+//       password : 'postgres',
+//       database : 'invoice_app'
+//     },
+//     pool: { min: 0, max: 7 }
+//   });
+
+// module.exports = knex;
+
 
 const knex = require('knex')({
     debug: true,
     client: 'pg',
     connection: {
-      host : 'localhost',
+      host : '34.65.174.239',
       user : 'postgres',
-      password : 'postgres',
-      database : 'invoice_app'
-    },
-    pool: { min: 0, max: 7 }
+      password : '8pM6oDOgbkyxuMH9',
+      database : 'ecommerce_staging'
+    }
   });
 
 module.exports = knex;
+
+
+// instance1 8pM6oDOgbkyxuMH9 https://cloud.google.com/sql/docs/postgres/connect-app-engine?hl=ru
+//  gcloud sql connect instance1 --user=postgres
