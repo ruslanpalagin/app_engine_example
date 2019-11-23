f07aa741175c842849da814ce819493d1047aced
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2019-11-23 15:03:18 +0200

    #email_spammer: prepare for deployment

--- client/src/services/http.js
+++ client/src/services/http.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isProduction = window.location.host.includes("appspot");
+
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: isProduction ? "https://docker-test-241719.appspot.com" : 'http://localhost:8080',
     timeout: 2000,
 });
 
